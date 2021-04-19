6ab39130d50a15daf0ef25055e5029920a0c6c5d
Francesco Michele Barranca <francescomich99@gmail.com>
2021-04-19 14:20:00 -0400

    added order model

---
+++ src/app/shared/models/order.ts
@@ -0,0 +1,77 @@
+export class Dish {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+    courseType: string;
+    isSpecial: boolean;
+
+    constructor({id, name, description, price, imageUrl, courseType, isSpecial}) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.courseType = courseType;
+        this.isSpecial = isSpecial;
+    }
+}
+
+export class Item {
+    product: Dish;
+    quantity: number;
+
+    constructor({product, quantity}) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    get subtotal(): number {
+        return this.product.price * this.quantity;
+    }
+}
+
+export class Order {
+    id: string;
+    clientname: string;
+    items: Item[];
+    email: string;
+    address: string;
+    phone: string;
+    pickup: boolean;
+    deliveryFees: number;
+    tip: number;
+    eta: number;
+    fulfilled: boolean;
+
+    constructor({id, clientname, items, email, address, phone, pickup, deliveryFees, tip, eta, fulfilled}) {
+        this.id = id;
+        this.clientname = clientname;
+        this.items = items;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+        this.pickup = pickup;
+        this.deliveryFees = deliveryFees;
+        this.tip = tip;
+        this.eta = eta;
+        this.fulfilled = fulfilled
+    }
+
+    get noTaxTotal(): number {
+        return this.items.reduce((prev, cur) => prev + cur.subtotal, 0.0);
+    }
+
+    get hst(): number {
+        return (this.noTaxTotal * 1.13) - this.noTaxTotal;
+    }
+
+    get chargedTip(): number {
+        return (this.noTaxTotal * (this.tip / 100 + 1));
+    }
+
+    get total(): number {
+        return this.noTaxTotal + this.hst + this.chargedTip + this.deliveryFees;
+    }
+}
